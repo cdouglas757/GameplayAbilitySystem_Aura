85c66086636a60855990516963cfb3157622323d
Cory Douglas <coryddouglas@gmail.com>
2023-10-05 20:55:10 -0400

    post process highlight

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -5,8 +5,15 @@
 
 void AAuraEnemy::HighlightActor()
 {
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(250.f);
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(250.f);
 }
 
 void AAuraEnemy::UnhighlightActor()
 {
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
+
 }
